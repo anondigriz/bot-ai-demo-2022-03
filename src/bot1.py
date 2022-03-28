605115907060453cdc153dc7f3f1854dae77471b
Dmitry Aladin <anon.digriz@gmail.com>
2022-03-28 19:08:39 +0300

    Создание первого бота

--- src/bot1.py
+++ src/bot1.py
@@ -1,8 +1,8 @@
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 from telegram import Update
 import logging
 import os
-# from recognizer import *
+from recognizer import *
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
@@ -16,9 +16,39 @@ def start(update: Update, context: CallbackContext):
         chat_id=update.effective_chat.id, text="Я бот, который умеет распозновать изображения цветов. Жду от тебя фотографию ❤️")
 
 
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Я жду от тебя фотографию 😄")
+
+
+def document(update: Update, context: CallbackContext):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Пришли, пожалуйста, сжатую фотографию 🤨")
+
+
+def photo(update: Update, context: CallbackContext):
+    full_path = None
+    prediction = "¯\_(ツ)_/¯"
+    try:
+        file = context.bot.getFile(update.message.photo[-1].file_id)
+        full_path = save_tmp_photo(file.file_path)
+        prediction = recognize_flower(full_path)
+    except Exception as ex:
+        print(ex)
+    finally:
+        if full_path != None:
+            remove_tmp_photo(full_path)
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=f"Я думаю, что это {prediction} 😍")
+
+
 if __name__ == "__main__":
     updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_handler(MessageHandler(Filters.document, document))
+    dispatcher.add_handler(MessageHandler(Filters.photo, photo))
+
     updater.start_polling()
